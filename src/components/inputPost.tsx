46b305c44b53192e4c4f51dc46a83bcda24dd182
MundaneLife <shshchoi9292@gmail.com>
2024-12-05 12:01:44 +0900

    ㅁㄴㅇㅁㄴㅇ

--- src/components/inputPost.tsx
+++ src/components/inputPost.tsx
@@ -77,7 +77,7 @@ const SubmitButton = styled.input`
   padding: 5px 10px;
   border-radius: 20px;
   border: none;
-  background-color: #ffffff;
+  background-color: rgba(14, 58, 234, 0.3);
   font-weight: bold;
   font-size: 12px;
   /* 마우스 올릴 때 */
@@ -246,7 +246,7 @@ export default () => {
                   </g>
                 </svg>
             ) : (
-                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13 3C13 2.44772 12.5523 2 12 2C11.4477 2 11 2.44772 11 3V11H3C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H11V21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21V13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H13V3Z" fill="#0F0F0F"></path> </g></svg>
+                "사진 업로드"
             )}
           </AttachPhotoButton>
           {/* accept 로 파일 포맷 지정 */}
